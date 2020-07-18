0b3bdcb6a22218ce0963290d1596420e263a2e0e
Urobhi <patomassaro@gmail.com>
2020-07-18 14:34:23 -0300

    csv data and p

--- DstCH/Data_Preparation.py
+++ DstCH/Data_Preparation.py
@@ -1,28 +1,27 @@
 import pandas as pd
 import os 
-import sys
+
 from os import listdir
-from pathlib import Path
 
 
 def rename_xls (filepaths):
     for f in filepaths:
         if f.endswith('.xls'):
-            os.rename(os.path.join(os.getcwd(),'Contacts',filepath),f.split('.')[0] + '.csv')
+            os.rename(os.path.join(os.getcwd(),'Contacts',f),f.split('.')[0] + '.csv')
 
 
 
 def load_pandascsv (filepaths):
     li = []
     for filepath in filepaths:
-        
+        url = 'https://raw.githubusercontent.com/Urobhi/BairesDev/master/DstCH/Contacts/'+ filepath
         if filepath.endswith(".csv"):
-            url = os.path.join('https://raw.githubusercontent.com/Urobhi/BairesDev/master/DstCH/Contacts', filepath)
+            
             aux=pd.read_csv(url,index_col = None)
+            
             aux['Response'] = filepath[0]
             li.append(aux)
         else:
-            url = os.path.join('https://raw.githubusercontent.com/Urobhi/BairesDev/master/DstCH/Contacts', filepath)
             aux=pd.read_excel(url, index_col = None)
             aux['Response'] = filepath[0]
             li.append(aux)
