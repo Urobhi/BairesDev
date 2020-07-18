a3b4c31e4575adf6ec4aa963941e86851f02a326
Urobhi <patomassaro@gmail.com>
2020-07-18 05:07:27 -0300

    ads

--- R_D Data Scientist Challenge/Data_Preparation.py
+++ R_D Data Scientist Challenge/Data_Preparation.py
@@ -1,6 +1,27 @@
-import os
-import glob
-import numpy as np
 import pandas as pd
+import os 
+import sys
+from os import listdir
 
 
+def rename_xls (filepaths):
+    for f in filepaths:
+        if f.endswith('.xls'):
+            os.rename(os.path.join(os.getcwd(),'Contacts',filepath),f.split('.')[0] + '.csv')
+
+
+filepaths = listdir("./Contacts")
+
+
+Data = pd.DataFrame()
+ for filepath in filepaths:
+     pdaux = pd.read_excel(os.path.join("./Contacts",filepath)
+     Data.append()
+def main():
+    data = []
+    
+    filepath = [f for f in  if f.startswith('N')]
+    for filename in files
+    with open (os.path.join(os.path.dirname(sys.argv[0]),))
+
+for f
